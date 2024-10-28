f6dd3c7ce9503c0d6e93db2b453ebfe86e86d6b4
Martie1668 <johnmartie.salvoro.cvt@eac.edu.ph>
2024-10-28 19:46:31 +0800

    viewrecipe.php

--- salvoro_OTM_PDO/viewrecipe.php
+++ salvoro_OTM_PDO/viewrecipe.php
@@ -1,23 +1,23 @@
+<?php require_once 'core/models.php'; ?>
+<?php require_once 'core/dbConfig.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<a href="index.php">Return to home</a>
-	<?php $getallinfobychefid = getallinfobychefid($_GET['chefid']); ?>
-	<h1>Chef name: <?php echo $getallinfobychefid['chefname']; ?></h1>
-	<h1>Add new recipe</h1>
+    <a href="index.php">Return to home</a>
+    <h1>Add new recipe</h1>
 	<form action="core/handleforms.php?chefid=<?php echo $_GET['chefid']; ?>" method="POST">
+
+    <p><label for="Recipename">Recipe Name </label> 
+            <input type="text" name="recipe"> </p>
 		<p>
-			<label for="recipename">Recipe Name: </label> 
-			<input type="text" name="recipename">
-		</p>
-		<p>
-			<label for="ingredients">Ingredients: </label> 
-			<input type="text" name="ingredients">
+			<label for="Ingredients">Ingredients</label> 
+			<input type="text" name="Recipepri">
 			<input type="submit" name="insertnewrecipebtn">
 		</p>
 	</form>
@@ -33,14 +33,15 @@
 	  <?php $getrecipebychef = getrecipebychef($pdo, $_GET['chefid']); ?>
 	  <?php foreach ($getrecipebychef as $row) { ?>
 	  <tr>
-	  	<td><?php echo $row['recipeid']; ?></td>	  	
-	  	<td><?php echo $row['recipename']; ?></td>	  	
-	  	<td><?php echo $row['ingredients']; ?></td>	  		  	
-	  	<td><?php echo $row['date_added']; ?></td>
-	  	<td>
-	  		<a href="editrecipe.php?recipeid=<?php echo $row['recipeid']; ?>&chefid=<?php echo $_GET['chefid']; ?>">Edit</a>
-	  		<a href="deleterecipe.php?recipeid=<?php echo $row['recipeid']; ?>&chefid=<?php echo $_GET['chefid']; ?>">Delete</a>
-	  	</td>	  	
+	  	    <td><?php echo $row['recipeid']; ?></td>	  	
+	  	    <td><?php echo $row['recipename']; ?></td>	  	
+	  	    <td><?php echo $row['ingredients']; ?></td>	  		  	
+	  	    <td><?php echo $row['date_added']; ?></td>
+	  	    <td>
+	  	<a href="editrecipe.php?recipeid=<?php echo $row['recipeid']; ?>&chefid=<?php echo $_GET['chefid']; ?>">Edit</a>
+
+	  	<a href="deleterecipe.php?recipeid=<?php echo $row['recipeid']; ?>&chefid=<?php echo $_GET['chefid']; ?>">Delete</a>
+	</td>	  	
 	  </tr>
 	<?php } ?>
 	</table>
