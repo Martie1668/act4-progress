5b625e49f064a0ddc428dfca8ebf4f447e8ca73e
Martie1668 <johnmartie.salvoro.cvt@eac.edu.ph>
2024-10-28 19:44:44 +0800

    models.php

--- salvoro_OTM_PDO/core/models.php
+++ salvoro_OTM_PDO/core/models.php
@@ -1,16 +1,19 @@
 <?php  
+// Insert chef
 
-function insertchef($pdo, $chefname, $specialization, $date_added,) {
+function insertchef($pdo, $chefname, $specialization) {
 
-	$sql = "INSERT INTO chef (chefname, specialization, date_added,) VALUES(?,?,?)";
+	$sql = "INSERT INTO chef (chefname, specialization) VALUES(?,?)";
 
 	$stmt = $pdo->prepare($sql);
-	$executeQuery = $stmt->execute([$chefname, $specialization, $date_added,]);
+	$executeQuery = $stmt->execute([$chefname, $specialization]);
 
 	if ($executeQuery) {
 		return true;
 	}
 }
+
+// get all chef
 function getallchef($pdo) {
 	$sql = "SELECT * FROM chef";
 	$stmt = $pdo->prepare($sql);
@@ -20,12 +23,27 @@ function getallchef($pdo) {
 		return $stmt->fetchAll();
 	}
 }
+
+function getchefbyid($pdo, $chefid) {
+	$sql = "SELECT * FROM chef WHERE chefid = ?";
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute([$chefid]);
+
+	if ($executeQuery) {
+		return $stmt->fetch();
+	}
+}
+
+
+
+
+// Update chef
 function updatechef($pdo, $chefname, $specialization, $chefid) {
 
 	$sql = "UPDATE chef
-				SET chefname = ?,
-					specialization = ?,
-				WHERE chefid = ?
+				        SET chefname = ?,
+					            specialization = ?
+				        WHERE chefid = ?
 			";
 	$stmt = $pdo->prepare($sql);
 	$executeQuery = $stmt->execute([$chefname, $specialization, $chefid]);
@@ -36,8 +54,9 @@ function updatechef($pdo, $chefname, $specialization, $chefid) {
 
 }
 
+// Delete chef
 function deletechef($pdo, $chefid) {
-	$deletechefrecipe = "DELETE FROM recipe WHERE userID = ?";
+	$deletechefrecipe = "DELETE FROM recipe WHERE chefid = ?";
 	$deleteStmt = $pdo->prepare($deletechefrecipe);
 	$executeDeleteQuery = $deleteStmt->execute([$chefid]);
 
@@ -52,6 +71,7 @@ function deletechef($pdo, $chefid) {
 
 	}
 	
+
 }
 
 function getrecipebychef($pdo, $chefid) {
@@ -60,8 +80,7 @@ function getrecipebychef($pdo, $chefid) {
 				recipe.recipeid AS recipeid,
 				recipe.recipename AS recipename,
 				recipe.ingredients AS ingredients,
-				recipe.date_added AS date_added,
-				chef.chefname AS chefname
+				chef.chefname AS recipename
 			FROM recipe
 			JOIN chef ON recipe.chefid = chef.chefid
 			WHERE recipe.chefid = ? 
@@ -74,6 +93,18 @@ function getrecipebychef($pdo, $chefid) {
 		return $stmt->fetchAll();
 	}
 }
+
+
+function insertrecipe($pdo, $recipename, $ingredients, $chefid) {
+	$sql = "INSERT INTO chef (recipename, ingredients, chefid) VALUES (?,?,?)";
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute([$recipename, $ingredients, $chefid]);
+	if ($executeQuery) {
+		return true;
+	}
+
+}
+
 function getrecipebyid($pdo, $recipeid) {
 	$sql = "SELECT 
 				recipe.recipeid AS recipeid,
@@ -92,6 +123,30 @@ function getrecipebyid($pdo, $recipeid) {
 		return $stmt->fetch();
 	}
 }
+
+function updaterecipe($pdo, $recipename, $ingredients, $recipeid) {
+	$sql = "UPDATE recipe
+			SET recipename = ?,
+				ingredients = ?
+			WHERE recipeid = ?
+			";
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute([$recipename, $ingredients, $recipeid]);
+
+	if ($executeQuery) {
+		return true;
+	}
+}
+
+function deleterecipe($pdo, $recipeid) {
+	$sql = "DELETE FROM recipe WHERE recipeid = ?";
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute([$recipeid]);
+	if ($executeQuery) {
+		return true;
+	}
+}
+
 
 
 ?>
